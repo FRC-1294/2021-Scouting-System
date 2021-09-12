7ff793f738e7739c0932f896f38a900a498f760c
Sam Tischaefer <samtisch@live.com>
2021-09-11 21:22:05 -0700

    Add colors to the start buttons

--- Start.Designer.cs
+++ Start.Designer.cs
@@ -38,7 +38,7 @@ namespace _1294_Scouting
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(126, 38);
+            this.label1.Location = new System.Drawing.Point(149, 138);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(420, 31);
             this.label1.TabIndex = 0;
@@ -47,22 +47,26 @@ namespace _1294_Scouting
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.button1.Location = new System.Drawing.Point(104, 216);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(169, 92);
             this.button1.TabIndex = 1;
             this.button1.Text = "Scouting";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(463, 225);
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(427, 216);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(202, 109);
+            this.button2.Size = new System.Drawing.Size(165, 92);
             this.button2.TabIndex = 2;
             this.button2.Text = "Server";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Start
