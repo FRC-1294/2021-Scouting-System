27a0b33ff88f8d7dcf1fc089532e98f45fb334fa
Sam Tischaefer <samtisch@live.com>
2021-09-15 09:06:34 -0700

    Create table

--- Server/Server.Designer.cs
+++ Server/Server.Designer.cs
@@ -29,24 +29,31 @@ namespace _1294_Scouting
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
-            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.Clients = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.AssignedTeam = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.teamTable = new System.Windows.Forms.TableLayoutPanel();
+            this.r1Input = new System.Windows.Forms.TextBox();
+            this.r1Label = new System.Windows.Forms.Label();
+            this.r1Team = new System.Windows.Forms.Label();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.sendArm = new System.Windows.Forms.CheckBox();
+            this.r2Input = new System.Windows.Forms.TextBox();
+            this.r3Input = new System.Windows.Forms.TextBox();
+            this.b1Input = new System.Windows.Forms.TextBox();
+            this.b2Input = new System.Windows.Forms.TextBox();
+            this.b3Input = new System.Windows.Forms.TextBox();
+            this.r2Label = new System.Windows.Forms.Label();
+            this.r3Label = new System.Windows.Forms.Label();
+            this.b1Label = new System.Windows.Forms.Label();
+            this.b2Label = new System.Windows.Forms.Label();
+            this.b3Label = new System.Windows.Forms.Label();
+            this.r2Team = new System.Windows.Forms.Label();
+            this.r3Team = new System.Windows.Forms.Label();
+            this.b1Team = new System.Windows.Forms.Label();
+            this.b2Team = new System.Windows.Forms.Label();
+            this.b3Team = new System.Windows.Forms.Label();
+            this.teamTable.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 76);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 26);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Mongo: OK\r\nSocket: OK";
             // 
             // textBox1
             // 
@@ -64,45 +71,253 @@ namespace _1294_Scouting
             this.button1.Text = "Get Data for This Team:";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // listView1
+            // teamTable
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.Clients,
-            this.Status,
-            this.AssignedTeam});
-            this.listView1.HideSelection = false;
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
-            this.listView1.Location = new System.Drawing.Point(401, 54);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(370, 172);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.teamTable.ColumnCount = 3;
+            this.teamTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.teamTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.teamTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.teamTable.Controls.Add(this.r1Label, 0, 0);
+            this.teamTable.Controls.Add(this.r1Team, 1, 0);
+            this.teamTable.Controls.Add(this.r2Input, 2, 1);
+            this.teamTable.Controls.Add(this.r3Input, 2, 2);
+            this.teamTable.Controls.Add(this.b1Input, 2, 3);
+            this.teamTable.Controls.Add(this.b2Input, 2, 4);
+            this.teamTable.Controls.Add(this.b3Input, 2, 5);
+            this.teamTable.Controls.Add(this.r2Label, 0, 1);
+            this.teamTable.Controls.Add(this.r3Label, 0, 2);
+            this.teamTable.Controls.Add(this.b1Label, 0, 3);
+            this.teamTable.Controls.Add(this.b2Label, 0, 4);
+            this.teamTable.Controls.Add(this.b3Label, 0, 5);
+            this.teamTable.Controls.Add(this.r2Team, 1, 1);
+            this.teamTable.Controls.Add(this.r3Team, 1, 2);
+            this.teamTable.Controls.Add(this.b1Team, 1, 3);
+            this.teamTable.Controls.Add(this.b2Team, 1, 4);
+            this.teamTable.Controls.Add(this.b3Team, 1, 5);
+            this.teamTable.Controls.Add(this.r1Input, 2, 0);
+            this.teamTable.Location = new System.Drawing.Point(412, 88);
+            this.teamTable.Name = "teamTable";
+            this.teamTable.RowCount = 6;
+            this.teamTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.teamTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.teamTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.teamTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.teamTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.teamTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.teamTable.Size = new System.Drawing.Size(261, 219);
+            this.teamTable.TabIndex = 3;
+            // 
+            // r1Input
+            // 
+            this.r1Input.Location = new System.Drawing.Point(136, 3);
+            this.r1Input.Name = "r1Input";
+            this.r1Input.Size = new System.Drawing.Size(100, 20);
+            this.r1Input.TabIndex = 0;
+            // 
+            // r1Label
+            // 
+            this.r1Label.AutoSize = true;
+            this.r1Label.Location = new System.Drawing.Point(3, 0);
+            this.r1Label.Name = "r1Label";
+            this.r1Label.Size = new System.Drawing.Size(36, 13);
+            this.r1Label.TabIndex = 4;
+            this.r1Label.Text = "Red 1";
+            // 
+            // r1Team
+            // 
+            this.r1Team.AutoSize = true;
+            this.r1Team.Location = new System.Drawing.Point(60, 0);
+            this.r1Team.Name = "r1Team";
+            this.r1Team.Size = new System.Drawing.Size(38, 13);
+            this.r1Team.TabIndex = 5;
+            this.r1Team.Text = "NONE";
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(328, 324);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(430, 79);
+            this.sendButton.TabIndex = 4;
+            this.sendButton.Text = "SEND NEXT MATCH";
+            this.sendButton.UseVisualStyleBackColor = true;
+            // 
+            // sendArm
+            // 
+            this.sendArm.AutoSize = true;
+            this.sendArm.Location = new System.Drawing.Point(206, 348);
+            this.sendArm.Name = "sendArm";
+            this.sendArm.Size = new System.Drawing.Size(112, 17);
+            this.sendArm.TabIndex = 5;
+            this.sendArm.Text = "Sending Disarmed";
+            this.sendArm.UseVisualStyleBackColor = true;
+            // 
+            // r2Input
+            // 
+            this.r2Input.Location = new System.Drawing.Point(136, 39);
+            this.r2Input.Name = "r2Input";
+            this.r2Input.Size = new System.Drawing.Size(100, 20);
+            this.r2Input.TabIndex = 6;
+            // 
+            // r3Input
+            // 
+            this.r3Input.Location = new System.Drawing.Point(136, 75);
+            this.r3Input.Name = "r3Input";
+            this.r3Input.Size = new System.Drawing.Size(100, 20);
+            this.r3Input.TabIndex = 7;
+            // 
+            // b1Input
+            // 
+            this.b1Input.Location = new System.Drawing.Point(136, 111);
+            this.b1Input.Name = "b1Input";
+            this.b1Input.Size = new System.Drawing.Size(100, 20);
+            this.b1Input.TabIndex = 8;
+            // 
+            // b2Input
+            // 
+            this.b2Input.Location = new System.Drawing.Point(136, 147);
+            this.b2Input.Name = "b2Input";
+            this.b2Input.Size = new System.Drawing.Size(100, 20);
+            this.b2Input.TabIndex = 9;
+            // 
+            // b3Input
+            // 
+            this.b3Input.Location = new System.Drawing.Point(136, 183);
+            this.b3Input.Name = "b3Input";
+            this.b3Input.Size = new System.Drawing.Size(100, 20);
+            this.b3Input.TabIndex = 10;
+            // 
+            // r2Label
+            // 
+            this.r2Label.AutoSize = true;
+            this.r2Label.Location = new System.Drawing.Point(3, 36);
+            this.r2Label.Name = "r2Label";
+            this.r2Label.Size = new System.Drawing.Size(36, 13);
+            this.r2Label.TabIndex = 11;
+            this.r2Label.Text = "Red 2";
+            // 
+            // r3Label
+            // 
+            this.r3Label.AutoSize = true;
+            this.r3Label.Location = new System.Drawing.Point(3, 72);
+            this.r3Label.Name = "r3Label";
+            this.r3Label.Size = new System.Drawing.Size(36, 13);
+            this.r3Label.TabIndex = 12;
+            this.r3Label.Text = "Red 3";
+            // 
+            // b1Label
+            // 
+            this.b1Label.AutoSize = true;
+            this.b1Label.Location = new System.Drawing.Point(3, 108);
+            this.b1Label.Name = "b1Label";
+            this.b1Label.Size = new System.Drawing.Size(37, 13);
+            this.b1Label.TabIndex = 13;
+            this.b1Label.Text = "Blue 1";
+            // 
+            // b2Label
+            // 
+            this.b2Label.AutoSize = true;
+            this.b2Label.Location = new System.Drawing.Point(3, 144);
+            this.b2Label.Name = "b2Label";
+            this.b2Label.Size = new System.Drawing.Size(37, 13);
+            this.b2Label.TabIndex = 14;
+            this.b2Label.Text = "Blue 2";
+            // 
+            // b3Label
+            // 
+            this.b3Label.AutoSize = true;
+            this.b3Label.Location = new System.Drawing.Point(3, 180);
+            this.b3Label.Name = "b3Label";
+            this.b3Label.Size = new System.Drawing.Size(37, 13);
+            this.b3Label.TabIndex = 15;
+            this.b3Label.Text = "Blue 3";
+            // 
+            // r2Team
+            // 
+            this.r2Team.AutoSize = true;
+            this.r2Team.Location = new System.Drawing.Point(60, 36);
+            this.r2Team.Name = "r2Team";
+            this.r2Team.Size = new System.Drawing.Size(38, 13);
+            this.r2Team.TabIndex = 16;
+            this.r2Team.Text = "NONE";
+            // 
+            // r3Team
+            // 
+            this.r3Team.AutoSize = true;
+            this.r3Team.Location = new System.Drawing.Point(60, 72);
+            this.r3Team.Name = "r3Team";
+            this.r3Team.Size = new System.Drawing.Size(38, 13);
+            this.r3Team.TabIndex = 17;
+            this.r3Team.Text = "NONE";
+            // 
+            // b1Team
+            // 
+            this.b1Team.AutoSize = true;
+            this.b1Team.Location = new System.Drawing.Point(60, 108);
+            this.b1Team.Name = "b1Team";
+            this.b1Team.Size = new System.Drawing.Size(38, 13);
+            this.b1Team.TabIndex = 18;
+            this.b1Team.Text = "NONE";
+            // 
+            // b2Team
+            // 
+            this.b2Team.AutoSize = true;
+            this.b2Team.Location = new System.Drawing.Point(60, 144);
+            this.b2Team.Name = "b2Team";
+            this.b2Team.Size = new System.Drawing.Size(38, 13);
+            this.b2Team.TabIndex = 19;
+            this.b2Team.Text = "NONE";
+            // 
+            // b3Team
+            // 
+            this.b3Team.AutoSize = true;
+            this.b3Team.Location = new System.Drawing.Point(60, 180);
+            this.b3Team.Name = "b3Team";
+            this.b3Team.Size = new System.Drawing.Size(38, 13);
+            this.b3Team.TabIndex = 20;
+            this.b3Team.Text = "NONE";
             // 
             // Server
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.sendArm);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.teamTable);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
             this.Name = "Server";
             this.Text = "Server";
+            this.teamTable.ResumeLayout(false);
+            this.teamTable.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader Clients;
-        private System.Windows.Forms.ColumnHeader Status;
-        private System.Windows.Forms.ColumnHeader AssignedTeam;
+        private System.Windows.Forms.TableLayoutPanel teamTable;
+        private System.Windows.Forms.Label r1Label;
+        private System.Windows.Forms.Label r1Team;
+        private System.Windows.Forms.TextBox r2Input;
+        private System.Windows.Forms.TextBox r3Input;
+        private System.Windows.Forms.TextBox b1Input;
+        private System.Windows.Forms.TextBox b2Input;
+        private System.Windows.Forms.TextBox b3Input;
+        private System.Windows.Forms.Label r2Label;
+        private System.Windows.Forms.Label r3Label;
+        private System.Windows.Forms.Label b1Label;
+        private System.Windows.Forms.Label b2Label;
+        private System.Windows.Forms.Label b3Label;
+        private System.Windows.Forms.Label r2Team;
+        private System.Windows.Forms.Label r3Team;
+        private System.Windows.Forms.Label b1Team;
+        private System.Windows.Forms.Label b2Team;
+        private System.Windows.Forms.Label b3Team;
+        private System.Windows.Forms.TextBox r1Input;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.CheckBox sendArm;
     }
 }
