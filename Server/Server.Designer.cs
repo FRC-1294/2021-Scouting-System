8419cc7ea5dc9f6c5195c8ecfdb0a1b610a524c6
Sam Tischaefer <samtisch@live.com>
2021-09-15 09:31:26 -0700

    Table, Send button, and safety.

--- Server/Server.Designer.cs
+++ Server/Server.Designer.cs
@@ -32,11 +32,8 @@ namespace _1294_Scouting
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.teamTable = new System.Windows.Forms.TableLayoutPanel();
-            this.r1Input = new System.Windows.Forms.TextBox();
             this.r1Label = new System.Windows.Forms.Label();
             this.r1Team = new System.Windows.Forms.Label();
-            this.sendButton = new System.Windows.Forms.Button();
-            this.sendArm = new System.Windows.Forms.CheckBox();
             this.r2Input = new System.Windows.Forms.TextBox();
             this.r3Input = new System.Windows.Forms.TextBox();
             this.b1Input = new System.Windows.Forms.TextBox();
@@ -52,19 +49,24 @@ namespace _1294_Scouting
             this.b1Team = new System.Windows.Forms.Label();
             this.b2Team = new System.Windows.Forms.Label();
             this.b3Team = new System.Windows.Forms.Label();
+            this.r1Input = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.sendArm = new System.Windows.Forms.CheckBox();
+            this.nextMatchBox = new System.Windows.Forms.GroupBox();
             this.teamTable.SuspendLayout();
+            this.nextMatchBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 207);
+            this.textBox1.Location = new System.Drawing.Point(589, 25);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(143, 20);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(65, 194);
+            this.button1.Location = new System.Drawing.Point(475, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 44);
             this.button1.TabIndex = 2;
@@ -76,7 +78,7 @@ namespace _1294_Scouting
             this.teamTable.ColumnCount = 3;
             this.teamTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57F));
             this.teamTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 76F));
-            this.teamTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.teamTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 128F));
             this.teamTable.Controls.Add(this.r1Label, 0, 0);
             this.teamTable.Controls.Add(this.r1Team, 1, 0);
             this.teamTable.Controls.Add(this.r2Input, 2, 1);
@@ -95,7 +97,7 @@ namespace _1294_Scouting
             this.teamTable.Controls.Add(this.b2Team, 1, 4);
             this.teamTable.Controls.Add(this.b3Team, 1, 5);
             this.teamTable.Controls.Add(this.r1Input, 2, 0);
-            this.teamTable.Location = new System.Drawing.Point(412, 88);
+            this.teamTable.Location = new System.Drawing.Point(6, 19);
             this.teamTable.Name = "teamTable";
             this.teamTable.RowCount = 6;
             this.teamTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
@@ -106,13 +108,6 @@ namespace _1294_Scouting
             this.teamTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.teamTable.Size = new System.Drawing.Size(261, 219);
             this.teamTable.TabIndex = 3;
-            // 
-            // r1Input
-            // 
-            this.r1Input.Location = new System.Drawing.Point(136, 3);
-            this.r1Input.Name = "r1Input";
-            this.r1Input.Size = new System.Drawing.Size(100, 20);
-            this.r1Input.TabIndex = 0;
             // 
             // r1Label
             // 
@@ -131,25 +126,6 @@ namespace _1294_Scouting
             this.r1Team.Size = new System.Drawing.Size(38, 13);
             this.r1Team.TabIndex = 5;
             this.r1Team.Text = "NONE";
-            // 
-            // sendButton
-            // 
-            this.sendButton.Location = new System.Drawing.Point(328, 324);
-            this.sendButton.Name = "sendButton";
-            this.sendButton.Size = new System.Drawing.Size(430, 79);
-            this.sendButton.TabIndex = 4;
-            this.sendButton.Text = "SEND NEXT MATCH";
-            this.sendButton.UseVisualStyleBackColor = true;
-            // 
-            // sendArm
-            // 
-            this.sendArm.AutoSize = true;
-            this.sendArm.Location = new System.Drawing.Point(206, 348);
-            this.sendArm.Name = "sendArm";
-            this.sendArm.Size = new System.Drawing.Size(112, 17);
-            this.sendArm.TabIndex = 5;
-            this.sendArm.Text = "Sending Disarmed";
-            this.sendArm.UseVisualStyleBackColor = true;
             // 
             // r2Input
             // 
@@ -276,20 +252,61 @@ namespace _1294_Scouting
             this.b3Team.TabIndex = 20;
             this.b3Team.Text = "NONE";
             // 
+            // r1Input
+            // 
+            this.r1Input.Location = new System.Drawing.Point(136, 3);
+            this.r1Input.Name = "r1Input";
+            this.r1Input.Size = new System.Drawing.Size(100, 20);
+            this.r1Input.TabIndex = 0;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Enabled = false;
+            this.sendButton.Location = new System.Drawing.Point(6, 286);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(261, 79);
+            this.sendButton.TabIndex = 4;
+            this.sendButton.Text = "disarmed";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // sendArm
+            // 
+            this.sendArm.AutoSize = true;
+            this.sendArm.Location = new System.Drawing.Point(6, 263);
+            this.sendArm.Name = "sendArm";
+            this.sendArm.Size = new System.Drawing.Size(89, 17);
+            this.sendArm.TabIndex = 5;
+            this.sendArm.Text = "Check to arm";
+            this.sendArm.UseVisualStyleBackColor = true;
+            this.sendArm.CheckedChanged += new System.EventHandler(this.sendArm_CheckedChanged);
+            // 
+            // nextMatchBox
+            // 
+            this.nextMatchBox.Controls.Add(this.sendArm);
+            this.nextMatchBox.Controls.Add(this.sendButton);
+            this.nextMatchBox.Controls.Add(this.teamTable);
+            this.nextMatchBox.Location = new System.Drawing.Point(12, 12);
+            this.nextMatchBox.Name = "nextMatchBox";
+            this.nextMatchBox.Size = new System.Drawing.Size(283, 377);
+            this.nextMatchBox.TabIndex = 6;
+            this.nextMatchBox.TabStop = false;
+            this.nextMatchBox.Text = "Next Match";
+            // 
             // Server
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.sendArm);
-            this.Controls.Add(this.sendButton);
-            this.Controls.Add(this.teamTable);
+            this.Controls.Add(this.nextMatchBox);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Server";
             this.Text = "Server";
             this.teamTable.ResumeLayout(false);
             this.teamTable.PerformLayout();
+            this.nextMatchBox.ResumeLayout(false);
+            this.nextMatchBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -319,5 +336,6 @@ namespace _1294_Scouting
         private System.Windows.Forms.TextBox r1Input;
         private System.Windows.Forms.Button sendButton;
         private System.Windows.Forms.CheckBox sendArm;
+        private System.Windows.Forms.GroupBox nextMatchBox;
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -16,5 +16,30 @@ namespace _1294_Scouting
         {
             InitializeComponent();
         }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            if(sendArm.Checked)
+            {
+                sendArm.Checked = false;
+                //TODO Send next match to clients
+            }
+        }
+
+        private void sendArm_CheckedChanged(object sender, EventArgs e)
+        {
+            if(sendArm.Checked)
+            {
+                sendButton.Enabled = true;
+                sendButton.Text = "SEND NEXT MATCH";
+                sendArm.Text = "ARMED";
+            } else
+            {
+                sendButton.Enabled = false;
+                sendButton.Text = "disarmed";
+                sendArm.Text = "Check to arm";
+            }
+            
+        }
     }
 }
